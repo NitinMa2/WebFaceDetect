359cba0c0b12a8dc9506902d3dfe40b427438bf2
NitinMa2 <nitinmathew00@gmail.com>
2018-11-18 15:43:13 +0530

    Updates

---
+++ precache-manifest.3d2d429e869be335446704e31b748592.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a31a38839ff62cb4255",
+    "url": "/WebFaceDetect/static/js/runtime~main.9a31a388.js"
+  },
+  {
+    "revision": "babadb2dd28694ec3315",
+    "url": "/WebFaceDetect/static/js/main.babadb2d.chunk.js"
+  },
+  {
+    "revision": "e881c267b6469487e6d9",
+    "url": "/WebFaceDetect/static/js/1.e881c267.chunk.js"
+  },
+  {
+    "revision": "babadb2dd28694ec3315",
+    "url": "/WebFaceDetect/static/css/main.077e401a.chunk.css"
+  },
+  {
+    "revision": "e881c267b6469487e6d9",
+    "url": "/WebFaceDetect/static/css/1.8fbc1bc6.chunk.css"
+  },
+  {
+    "revision": "dc17be3b86b7797553510f5169f7b441",
+    "url": "/WebFaceDetect/index.html"
+  }
+];
